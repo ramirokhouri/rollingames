1fa7ad5f16cbb03f836439c6b3276930653b8a48
ramirokhouri <ramirokhouri@hotmail.com>
2023-07-28 12:10:04 -0300

    links navbar

--- js/categoria.js
+++ js/categoria.js
@@ -44,4 +44,16 @@ function cargarJuegos() {
   });
 }
 
+function cargarCategorias_nav() {
+  const ul_categorias = document.getElementById("ul_categorias");
+  const generos = ["Accion", "Aventura", "FPS", "Lucha", "RPG", "Disparos"];
+  generos.forEach((genero, index) => {
+    const li = document.createElement("li");
+    li.setAttribute("class", "nav-item");
+    li.innerHTML = `<a class="nav-link active" aria-current="page" href="${`./categoria.html?genero=${genero}`}">${genero}</a>`;
+    ul_categorias.appendChild(li);
+  });
+}
+
 cargarJuegos();
+cargarCategorias_nav();
--- js/index.js
+++ js/index.js
@@ -99,5 +99,16 @@ function mostrarCategoria(genero) {
   window.location.href = `./html/categoria.html?genero=${generos[genero]}`;
 }
 
+function cargarCategorias_nav() {
+  const ul_categorias = document.getElementById("ul_categorias");
+  generos.forEach((genero, index) => {
+    const li = document.createElement("li");
+    li.setAttribute("class", "nav-item");
+    li.innerHTML = `<a class="nav-link active" aria-current="page" href="${`./html/categoria.html?genero=${genero}`}">${genero}</a>`;
+    ul_categorias.appendChild(li);
+  });
+}
+
+cargarCategorias_nav();
 cargarJuegos();
 cargarCarrusel();
