39404f80f5cdfb47f40af0f3a2aee80690dd1e4b
kuori <ramirokhouri@hotmail.com>
2023-07-30 13:27:33 -0300

    detalle juego dinámico listo

--- js/detalle_juego.js
+++ js/detalle_juego.js
@@ -6,9 +6,10 @@ function cargarJuego(){
     let url_string = window.location.href;
     let url = new URL(url_string);
     let juegoID = url.searchParams.get("juegoID");
-    console.log(juegoID);
 
+    // objeto juego a partir del juegoID
     let juego = videojuegos.find((juego) => juego.id == juegoID );
+    let generos_str = juego.genre.join(", ");
 
     let div = document.createElement("div");
     div.innerHTML = `
@@ -41,36 +42,31 @@ function cargarJuego(){
     </div>
     `
     juego_container.appendChild(div);
-    
+
+    let div_contenido = document.createElement("div");
+    div_contenido.setAttribute("class", "contenido");
+    div_contenido.innerHTML = `
+    <img src="${juego.icon}">
+    <h1 class="nombre-juego display-5 pt-3">${juego.title}</h1>
+    <h3 class="descripcion pt-3">${generos_str}</h3>
+    <p class="descripcion lead">${juego.description}</p>
+    <p class="descripcion">Desarrollado por ${juego.developer}</p>
+    <button type="button" class="btn btn-primary btn-lg">$${juego.price}</button></h1>
+
+    `
+    juego_container.appendChild(div_contenido);
 }
 
-cargarJuego();
+function cargarCategorias_nav() {
+    const ul_categorias = document.getElementById("ul_categorias");
+    const generos = ["Accion", "Aventura", "FPS", "Lucha", "RPG", "Disparos"];
+    generos.forEach((genero, index) => {
+      const li = document.createElement("li");
+      li.setAttribute("class", "nav-item");
+      li.innerHTML = `<a class="nav-link active" aria-current="page" href="${`./categoria.html?genero=${genero}`}">${genero}</a>`;
+      ul_categorias.appendChild(li);
+    });
+  }
 
-/*
-<section class="detalle-juego">
-    <div class="contenido">
-        <h1 class="nombre-juego">Resident Evil 4</h1>
-        <p class="descripcion" style="color: #FFB400;">Un jugador</p>
-        <h3>$15,500.00</h2>
-        <br>
-        <button type="button" class="btn btn-primary btn-lg">Comprar ahora</button>
-        <br>
-        <br>
-        <h2 class="titulo">Descripción</h1>
-        <p class="descripcion">
-            Resident Evil 4 es un remake del Resident Evil 4 original del 2005.
-            Survival Horror de última generación reimaginado para el año 2023.
-            Resident Evil 4 conserva la esencia del juego original, a la vez que introduce mecánicas de juego actualizadas,una historia reimaginada, e impresionantes gráficos de última generación que lo convierten en la experiencia de Survival Horror definitiva en la cual se cruzan la vida y la muerte, el terror y la catarsis.
-        </p>
-        <br>
-        <h2 class="titulo">Historia</h1>
-        <p class="descripcion">
-            Han pasado seis años desde la catástrofe biológica de Raccoon City.
-            Leon S. Kennedy, uno de los supervivientes, ha sido asignado como agente especial del gobierno al presidente de los Estados Unidos.
-            Con la experiencia de múltiples misiones a sus espaldas,Leon es enviado a rescatar a la hija del presidente, la cual ha sido secuestrada.
-            Sus pesquisas le llevan a un aislado pueblo europeo,en donde algo muy extraño está pasando con sus habitantes...
-            El telón se alza desvelando una cruenta experiencia de Survival Horror y la historia de un angustioso rescate.
-        </p>
-    </div>
-</section>
-*/
+cargarJuego();
+cargarCategorias_nav();
