a7ef885903e84e449072a6f49dfe0f76b5330284
kuori <ramirokhouri@hotmail.com>
2023-07-30 22:06:15 -0300

    admin: botones destacado si/no y delete funcionando

--- js/admin.js
+++ js/admin.js
@@ -3,6 +3,21 @@ const generos = ["Accion", "Aventura", "FPS", "Lucha", "RPG", "Disparos"];
 const admin_container = document.getElementById("admin-container");
 const tabla_juegos = document.getElementById("tabla-juegos");
 
+function destacado(juego_id, n) {
+    let juego = videojuegos.find((juego) => juego.id == juego_id );
+    console.log("featured antes: " + juego.featured);
+    n === 1 ? juego.featured = true : juego.featured = false;
+    console.log("featured después: " + juego.featured);
+    localStorage.setItem("juegos", JSON.stringify(videojuegos));
+    
+}
+
+function borrarJuego(i){
+    videojuegos.splice(i,1);
+    localStorage.setItem("juegos", JSON.stringify(videojuegos));
+    cargarJuegos();
+}
+
 function cargarCategorias_nav() {
     const ul_categorias = document.getElementById("ul_categorias");
     generos.forEach((genero, index) => {
@@ -25,14 +40,31 @@ function cargarJuegos() {
         <td>${juego.published}</td>
         <td>
             <div class="btn-group">
-                <a href="#" class="btn btn-success"><i class="bi bi-star-fill"></i></a>
-                <a href="#" class="btn btn-info text-white"><i class="bi bi-pencil-square"></i></a>
-                <a href="#" class="btn btn-danger"><i class="bi bi-trash-fill"></i></a>
+                <button type="button" class="btn btn-success" onclick="destacado('${juego.id}',1)"><i class="bi bi-star-fill"></i></button>
+                <button type="button" class="btn btn-secondary" onclick="destacado('${juego.id}',0)"><i class="bi bi-star"></i></button>
+                <button type="button" class="btn btn-info text-white"><i class="bi bi-pencil-square"></i></button>
+                <button type="button" class="btn btn-danger" onclick="borrarJuego(${index})"><i class="bi bi-trash-fill"></i></button>
             </div>
         </td>
         `
         tabla_juegos.appendChild(tr);
     });
+}
+
+function destacado(juego_id, n) {
+    let juego = videojuegos.find((juego) => juego.id == juego_id );
+    console.log("featured antes: " + juego.featured);
+    n === 1 ? juego.featured = true : juego.featured = false;
+    console.log("featured después: " + juego.featured);
+    localStorage.setItem("juegos", JSON.stringify(videojuegos));
+    
+}
+
+function borrarJuego(i){
+    videojuegos.splice(i,1);
+    localStorage.setItem("juegos", JSON.stringify(videojuegos));
+    cargarJuegos();
+    window.location.reload();
 }
 
 cargarCategorias_nav();
